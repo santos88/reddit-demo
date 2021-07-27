8da451ea958ddae60627cd2370a175783ff42bb5
Santos Ramon <santos.ramon.brito@gmail.com>
2021-07-27 17:02:38 -0500

    Add Article Model with codable

---
+++ RedditDemo/RedditDemo/Model/ArticleModel.swift
@@ -0,0 +1,39 @@
+//
+//  ArticleModel.swift
+//  RedditDemo
+//
+//  Created by Santos Ramon on 7/27/21.
+//
+
+import Foundation
+
+struct ArticleModel:Codable, Equatable {
+    var title:String
+    var author:String
+    var created:Date
+    var thumbnail:String
+    var num_comments:Int
+    var wasRead:Bool?
+    
+    static func ==(lhs: ArticleModel, rhs: ArticleModel) -> Bool {
+        let areEqual = lhs.title == rhs.title && lhs.author == rhs.author && lhs.created == rhs.created
+        return areEqual
+    }
+}
+
+struct ArticlesServerResponse: Codable {
+    struct ArticlesData: Codable {
+        struct Children: Codable {
+            var kind:String
+            var data:ArticleModel
+        }
+        var children : [Children]
+    }
+    var data:ArticlesData
+    var kind:String
+    
+    func articlesArray() -> [ArticleModel] {
+        return data.children.map{ $0.data }
+    }
+    
+}
