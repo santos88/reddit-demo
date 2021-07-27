d1d7d905836f4f5cd24264c43f8196180d85bffc
Santos Ramon <santos.ramon.brito@gmail.com>
2021-07-27 17:02:08 -0500

    Reorder folders

---
+++ RedditDemo/RedditDemo/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditDemo
+//
+//  Created by Santos Ramon on 7/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
